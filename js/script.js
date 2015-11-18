4ffc6010ab39935835224faa7151e696dcf48932
jmona789 <jmona789@gmail.com>
2015-11-18 01:43:42 -0500

    added code to make delete buttons work

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 var newItem = document.getElementById("newItem");
 document.getElementById("addItemBtn").addEventListener("click", addItem);
 var deleteButtons = document.getElementsByClassName("btn-danger")
+var newItemNum = 5;
+
 for(var i = 0; i < deleteButtons.length; i++) {
-  deleteButtons[i].addEventListener("click", function() {
-    alert("clicky");
+  deleteButtons[i].addEventListener("click", function(e) {
+    var raw = document.getElementById(e.target.getAttribute("rawLink"));
+    raw.parentNode.removeChild(raw);
     });
 }
 
@@ -11,7 +14,15 @@ function addItem(){
   var ul = document.getElementById("toDoList");
   var li = document.createElement("li");
   ul.appendChild(li);
-  li.innerHTML = '<input type="checkbox" />'+' '+newItem.value+' '+'<button class="btn btn-danger btn-xs">Delete</button>'
+  li.innerHTML = '<input type="checkbox" />'+' '+newItem.value+' '+'<button rawLink="r'+newItemNum+'" value="Delete"class="btn btn-danger btn-xs">Delete</button>';
+  li.setAttribute("id", "r"+newItemNum);
+  newItemNum ++;
+  for(var i = 0; i < deleteButtons.length; i++) {
+  deleteButtons[i].addEventListener("click", function(e) {
+    var raw = document.getElementById(e.target.getAttribute("rawLink"));
+    raw.parentNode.removeChild(raw);
+    });
+  }
   document.getElementById("newItemForm").reset();
 }
 
